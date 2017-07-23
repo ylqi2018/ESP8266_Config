5d39bf70c1df06acde04b8ece2589b249ebb4543
ylqi007 <yunlongqi@my.unt.edu>
2017-07-23 11:27:30 -0500

    Version 0.1, Send OK, Received OK, ERROR, busy p...

---
+++ ESP8266_Config_0.1/ESP8266_Config_0.1.runs/impl_1/ESP8266_ClientConfig.tcl
@@ -0,0 +1,65 @@
+proc start_step { step } {
+  set stopFile ".stop.rst"
+  if {[file isfile .stop.rst]} {
+    puts ""
+    puts "*** Halting run - EA reset detected ***"
+    puts ""
+    puts ""
+    return -code error
+  }
+  set beginFile ".$step.begin.rst"
+  set platform "$::tcl_platform(platform)"
+  set user "$::tcl_platform(user)"
+  set pid [pid]
+  set host ""
+  if { [string equal $platform unix] } {
+    if { [info exist ::env(HOSTNAME)] } {
+      set host $::env(HOSTNAME)
+    }
+  } else {
+    if { [info exist ::env(COMPUTERNAME)] } {
+      set host $::env(COMPUTERNAME)
+    }
+  }
+  set ch [open $beginFile w]
+  puts $ch "<?xml version=\"1.0\"?>"
+  puts $ch "<ProcessHandle Version=\"1\" Minor=\"0\">"
+  puts $ch "    <Process Command=\".planAhead.\" Owner=\"$user\" Host=\"$host\" Pid=\"$pid\">"
+  puts $ch "    </Process>"
+  puts $ch "</ProcessHandle>"
+  close $ch
+}
+
+proc end_step { step } {
+  set endFile ".$step.end.rst"
+  set ch [open $endFile w]
+  close $ch
+}
+
+proc step_failed { step } {
+  set endFile ".$step.error.rst"
+  set ch [open $endFile w]
+  close $ch
+}
+
+set_msg_config -id {HDL 9-1061} -limit 100000
+set_msg_config -id {HDL 9-1654} -limit 100000
+
+start_step write_bitstream
+set rc [catch {
+  create_msg_db write_bitstream.pb
+  set_param xicom.use_bs_reader 1
+  open_checkpoint ESP8266_ClientConfig_routed.dcp
+  set_property webtalk.parent_dir D:/Vivado_Program_Files/20170711_ESP8266/ESP8266_Config_0.1/ESP8266_Config_0.1.cache/wt [current_project]
+  catch { write_mem_info -force ESP8266_ClientConfig.mmi }
+  write_bitstream -force ESP8266_ClientConfig.bit 
+  catch { write_sysdef -hwdef ESP8266_ClientConfig.hwdef -bitfile ESP8266_ClientConfig.bit -meminfo ESP8266_ClientConfig.mmi -file ESP8266_ClientConfig.sysdef }
+  close_msg_db -file write_bitstream.pb
+} RESULT]
+if {$rc} {
+  step_failed write_bitstream
+  return -code error $RESULT
+} else {
+  end_step write_bitstream
+}
+
