7fabb569c4870c81879c2f2a0ec0258e6152bf2b
ylqi007 <yunlongqi@my.unt.edu>
2017-08-16 14:36:49 -0500

    Add status which end up with Carriage Return

---
+++ ESP8266_Config_2.1.1/ESP8266_Config_2.1.1.srcs/sources_1/Top_Receiver.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 08/14/2017 04:50:25 PM
+-- Design Name: 
+-- Module Name: Top_Receiver - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Top_Receiver is
+--  Port ( );
+end Top_Receiver;
+
+architecture Behavioral of Top_Receiver is
+
+begin
+
+
+end Behavioral;
