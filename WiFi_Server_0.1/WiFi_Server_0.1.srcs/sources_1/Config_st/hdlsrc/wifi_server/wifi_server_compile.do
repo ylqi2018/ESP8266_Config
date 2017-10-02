270d3129faae2298aeb33d567958569227a1a98a
ylqi007 <yunlongqi@my.unt.edu>
2017-10-02 13:34:52 -0500

    wifi server 0.1

---
+++ WiFi_Server_0.1/WiFi_Server_0.1.srcs/sources_1/Config_st/hdlsrc/wifi_server/wifi_server_compile.do
@@ -0,0 +1,4 @@
+vlib work
+vcom  wifi_server_pkg.vhd
+vcom  wifi_setup_server_mac.vhd
+vcom  wifi_server.vhd
---
+++ WiFi_Server_0.1/WiFi_Server_0.1.srcs/sources_1/ip/ila_server/ila_server_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2015.4 (win64) Build 1412921 Wed Nov 18 09:43:45 MST 2015
+-- Date        : Fri Sep 29 13:08:24 2017
+-- Host        : MS-20160829GDVC running 64-bit Service Pack 1  (build 7601)
+-- Command     : write_vhdl -force -mode synth_stub
+--               d:/Vivado_Program_Files/20170711_ESP8266/WiFi_Server_0.1/WiFi_Server_0.1.srcs/sources_1/ip/ila_server/ila_server_stub.vhdl
+-- Design      : ila_server
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity ila_server is
+  Port ( 
+    clk : in STD_LOGIC;
+    probe0 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe1 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe2 : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    probe3 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe4 : in STD_LOGIC_VECTOR ( 7 downto 0 )
+  );
+
+end ila_server;
+
+architecture stub of ila_server is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk,probe0[0:0],probe1[0:0],probe2[7:0],probe3[0:0],probe4[7:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "ila,Vivado 2015.4";
+begin
+end;
